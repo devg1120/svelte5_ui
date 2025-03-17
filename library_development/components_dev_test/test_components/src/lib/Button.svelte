a328708ad4995d614dd04082a5ab0e7a06892672
root <root@ubuntu-server-01.myguest.virtualbox.org>
2025-03-17 17:02:10 +0900

    up

---
+++ library_development/components_dev_test/test_components/src/lib/Button.svelte
@@ -0,0 +1,32 @@
+
+<script>
+  /** @type {'default' | 'pill' | 'outline'} */
+  export let style = 'default';
+
+  /** @type {string} */
+  export let href = null;
+</script>
+
+{#if href}
+  <a {href}> 
+   <button class={style}>リンクボタン</button>
+  </a>
+{:else}
+  <button class={style}>ボタン</button>
+{/if}
+
+<style>
+  .default {
+    @apply bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded;
+  }
+
+  .pill {
+    @apply bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full;
+  }
+
+  .outline {
+    @apply bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded;
+  }
+</style>
+
+
